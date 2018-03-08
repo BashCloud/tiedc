349e26986aeb62517ae965386cb6da9f1e8314e9
Aditya Mohan <adityamohan885@gmail.com>
2018-03-08 22:11:55 +0530

    added Aditya component

--- src/components/Aditya.vue
+++ src/components/Aditya.vue
@@ -1,15 +1,24 @@
 <template>
-  <div>
-      HEllo
+  <div class="initiatives">
+      <h1>{{msg}}</h1>
+
   </div>
 </template>
 
 <script>
 export default {
+    name: 'Aditya',
+    data(){
+        return{
+            msg: "Our Initiatives"
+        }
+    }
 
 }
 </script>
 
 <style>
-
+h1,h2{
+    font-weight: normal;
+}
 </style>
