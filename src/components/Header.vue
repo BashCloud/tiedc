d841241825b9e129e3d500da60d25a1452fb87a2
Sasha303 <srishtigupta303@gmail.com>
2018-03-08 19:37:09 +0530

    "added component Srishti"

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,6 +4,7 @@
       <div class="navLinks">
         <router-link to="/">Home</router-link>
         <router-link to="/about">About</router-link>
+        <router-link to="/new">new</router-link>
       </div>
   </div>
 </template>
--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import Home from "@/components/Home";
-
+import Srishti from "@/components/Srishti";
 Vue.use(Router)
 
 export default new Router({
@@ -16,6 +16,11 @@ export default new Router({
             path: '/',
             name: 'Home',
             component: Home
+        },
+        {
+            path: '/new',
+            name: 'newname',
+            component: Srishti
         }
     ]
 })
