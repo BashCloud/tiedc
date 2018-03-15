2681003cd2a78b705fe92bf87d9061deaa94fd72
Aditya Mohan <adityamohan885@gmail.com>
2018-03-15 18:54:36 +0530

    events add

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,9 +1,10 @@
 <template>
   <div class="mainNav">
-      <img class="logo" src="../assets/TIEDC_Logo.png" /> 
+       
       <div class="navLinks">
         <router-link to="/">Home</router-link>
         <router-link to="/about">About</router-link>
+        <img class="logo" src="../assets/TIEDC_Logo.png" />
         <router-link to="/new">new</router-link>
         <router-link to="/aditya">Aditya</router-link>
       </div>
@@ -19,14 +20,14 @@ export default {
 <style>
 .mainNav{
   height: 60px;
-  background-color: #ccc;
+  background: #dedede;
   padding: 10px;
 }
 .mainNav .logo{
   height: 60px;
 }
 .mainNav .navLinks{
-  float: right;
+  float: center;
   text-decoration: none;
 }
 </style>
